839d8725e41820620df8e55a5302528410fb6b35
Charles Lelis Braga <charles.braga.jf.cb@gmail.com>
2023-06-15 14:16:22 -0300

    feat: nav

--- src/components/Molecules/DropdownNav/index.tsx
+++ src/components/Molecules/DropdownNav/index.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Container } from './styles';
 
 interface Props {
   label: string;
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const DropdownNav: React.FC<Props> = ({ label }) => {
-  return <Container>{label}</Container>;
+const DropdownNav: React.FC<Props> = ({ label, open, setOpen }) => {
+  const [show, setShow] = useState<boolean>(false);
+
+  return (
+    <Container onClick={() => setOpen(!open)} onMouseEnter={() => setShow(true)} onMouseLeave={() => setShow(false)}>
+      {label}
+    </Container>
+  );
 };
 
 export default DropdownNav;
