53301d54acbcfac08c350e3d3ca50da0970678de
Gabriella Carvalho <gabriellacarvalho0701@gmail.com>
2023-06-17 11:47:53 -0300

    feature: Desenvolvimento do addLink

--- src/helpers/Automaton/Links/AddLink/index.ts
+++ src/helpers/Automaton/Links/AddLink/index.ts
@@ -1,8 +1,41 @@
-import { IAutomaton } from '@/@types/components/Automaton';
-import { LinkObject } from 'react-force-graph-2d';
+import { IAutomaton, INode } from '@/@types/components/Automaton';
 import findNodeById from '../../Nodes/FindNodeById';
+import findLinkByNodeId from '../FindLinkByNodesId';
 
-const addLink = (automaton: IAutomaton, idNodeSource: number, idNodeTarget: number, name?: string) => {
+const addDataToCreatedLink = (automaton: IAutomaton, nodeSource: INode, nodeTarget: INode, name: string): IAutomaton => {
+  const link = automaton.links.find((link) => link.source === nodeSource && link.target === nodeTarget);
+
+  //@ts-ignore
+  link.name = `${link?.name}, ${name}`;
+
+  return automaton;
+};
+
+const createLink = (automaton: IAutomaton, nodeSource: INode, nodeTarget: INode, name: string): IAutomaton => {
+  return {
+    ...automaton,
+    links: [
+      ...automaton.links,
+      {
+        source: nodeSource,
+        target: nodeTarget,
+        name: name
+      }
+    ]
+  };
+};
+
+const verifyLinkExists = (automaton: IAutomaton, nodeSource: INode, nodeTarget: INode): boolean => {
+  const link = findLinkByNodeId(automaton, nodeSource, nodeTarget);
+
+  if (!link) {
+    return false;
+  }
+
+  return true;
+};
+
+const addLink = (automaton: IAutomaton, idNodeSource: number, idNodeTarget: number, name: string): IAutomaton => {
   const nodeSource = findNodeById(automaton, idNodeSource);
   if (!nodeSource) {
     throw new Error('Não existe nó cabeça com esse ID');
@@ -13,13 +46,11 @@ const addLink = (automaton: IAutomaton, idNodeSource: number, idNodeTarget: numb
     throw new Error('Não existe nó cauda com esse ID');
   }
 
-  automaton.links.push({
-    source: nodeSource,
-    target: nodeTarget,
-    name: name
-  });
+  const newAtomaton = verifyLinkExists(automaton, nodeSource, nodeTarget)
+    ? addDataToCreatedLink(automaton, nodeSource, nodeTarget, name)
+    : createLink(automaton, nodeSource, nodeTarget, name);
 
-  return automaton;
+  return newAtomaton;
 };
 
 export default addLink;
