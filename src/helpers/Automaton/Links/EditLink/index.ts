e2ce4a30f8228a983201719775c2488d4a46b267
Gabriella Carvalho <gabriellacarvalho0701@gmail.com>
2023-06-17 12:11:59 -0300

    feature: Desenvolvimento do editLink

---
+++ src/helpers/Automaton/Links/EditLink/index.ts
@@ -0,0 +1,32 @@
+import { IAutomaton, ILink } from '@/@types/components/Automaton';
+import findNodeById from '../../Nodes/FindNodeById';
+import findLinkByNodesId from '../FindLinkByNodesId';
+
+const editLink = (automaton: IAutomaton, idNodeSource: number, idNodeTarget: number, name: string): IAutomaton => {
+  const nodeSource = findNodeById(automaton, idNodeSource);
+  if (!nodeSource) {
+    throw new Error('Não existe nó cabeça com esse ID');
+  }
+
+  const nodeTarget = findNodeById(automaton, idNodeTarget);
+  if (!nodeTarget) {
+    throw new Error('Não existe nó cauda com esse ID');
+  }
+
+  const link = findLinkByNodesId(automaton, nodeSource, nodeTarget);
+
+  for (let i = 0; i < link?.name.length; i++) {
+    const splitName = link?.name.split(', ');
+    if (splitName[i] == name) {
+      console.log(splitName[i], name);
+      return automaton;
+    }
+  }
+
+  //@ts-ignore
+  link.name = `${link?.name}, ${name}`;
+
+  return automaton;
+};
+
+export default editLink;
