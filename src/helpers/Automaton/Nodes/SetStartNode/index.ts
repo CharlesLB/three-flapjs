004a176f3f26d54699c7d2e665ef7cbf32605c4f
Gabriella Carvalho <gabriellacarvalho0701@gmail.com>
2023-06-17 21:18:59 -0300

    feature: Funções para identificar os nós

---
+++ src/helpers/Automaton/Nodes/SetStartNode/index.ts
@@ -0,0 +1,16 @@
+import { IAutomaton, INode } from '@/@types/components/Automaton';
+import findNodeById from '../FindNodeById';
+
+const setStartNode = (automaton: IAutomaton, id: number): IAutomaton => {
+  const node = findNodeById(automaton, id);
+
+  if (!node) {
+    throw new Error('Node does not exist');
+  }
+
+  node.start = true;
+
+  return automaton;
+};
+
+export default setStartNode;
